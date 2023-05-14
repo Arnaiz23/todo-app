e206898d77e6bfe6f3e9d7fbdd707419b195d7f0
Arnaiz23 <aaf110501@gmail.com>
2023-05-14 17:43:06 +0200

    feat: 🎸 Create new todo and get one todo for id

--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -1,19 +1,43 @@
 import { getConnection } from "../database.js"
 
+// TODOS
+
 async function getAllTodos() {
 	try {
 		const con = await getConnection()
 		const [rows] = await con.query(`SELECT * FROM todos`)
-		// const query = await pool.query(`SELECT * FROM todos`)
-		// return res.json(rows)
-		return rows
+		return { rows }
 	} catch (err) {
-		console.log(err)
-		return
-		// return res.json({
-		// 	status: "error",
-		// })
+		console.log("Get todos " + err)
+		return { err }
 	}
 }
 
-export { getAllTodos }
+async function newTodo({ title }) {
+	try {
+		const con = await getConnection()
+		const [result] = await con.query(`INSERT INTO todos (title) VALUES (?)`, [
+			title,
+		])
+
+		const newId = result.insertId
+		const { row } = await getTodoWithId(newId)
+		return { row }
+	} catch (err) {
+		console.log("Insert todo " + err)
+		return { err }
+	}
+}
+
+async function getTodoWithId(id) {
+	try {
+		const con = await getConnection()
+		const [row] = await con.query(`SELECT * FROM todos WHERE id=?`, [id])
+		return { row }
+	} catch (err) {
+		console.log("Get Todo with id: " + err)
+		return { err }
+	}
+}
+
+export { getAllTodos, newTodo }
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,19 +1,47 @@
 import { Router } from "express"
-import { getAllTodos } from "../controllers/controllers.js"
+import { getAllTodos, newTodo } from "../controllers/controllers.js"
 
 const router = Router()
 
 router.get("/", (req, res) => {
-	const routes = {
-		"/todos": "Get all the TODOS",
-	}
+	const routes = [
+		{
+			method: "GET",
+			route: "/todos",
+			description: "Get all the TODOS",
+		},
+		{
+			method: "POST",
+			route: "/todos",
+			description: "Create a new TODO",
+		},
+	]
 
 	return res.json(routes)
 })
 
 router.get("/todos", async (req, res) => {
-	const rows = await getAllTodos()
+	const { rows, err } = await getAllTodos()
+
+	if (err) {
+		return res.status(404).json({
+			Error: "Error with the SELECT query of the todos table",
+		})
+	}
 	return res.json(rows)
+})
+
+router.post("/todos", async (req, res) => {
+	const { title } = req.body
+	const { row, err } = await newTodo({ title })
+
+	if (err) {
+		return res.status(404).json({
+			Error: "Error with the INSERT query of the todos table",
+		})
+	}
+
+	return res.json(row)
 })
 
 export { router }

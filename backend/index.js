bba8ac72b16025a674ccba791385056bcb46c0c2
Arnaiz23 <aaf110501@gmail.com>
2023-05-14 14:37:57 +0200

    feat: 🎸 Create the basic get petition

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 
+import { router } from "./src/routes/routes.js"
+
 dotenv.config()
 
 const app = express()
 const PORT = process.env.BACKEND_PORT
 
 app.use(cors())
+app.use(express.json())
+app.use("/api", router)
 
 app.listen(PORT, () => {
 	console.log(`The server is listening in the port: ${PORT}`)

643ed499097a5adf88f81d2b239363a31cdd60a5
Arnaiz23 <aaf110501@gmail.com>
2023-05-15 12:55:57 +0200

    feat: 🎸 Delete todo with Id

--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -3,41 +3,52 @@ import { getConnection } from "../database.js"
 // TODOS
 
 async function getAllTodos() {
-	try {
-		const con = await getConnection()
-		const [rows] = await con.query(`SELECT * FROM todos`)
-		return { rows }
-	} catch (err) {
-		console.log("Get todos " + err)
-		return { err }
-	}
+  try {
+    const con = await getConnection()
+    const [rows] = await con.query(`SELECT * FROM todos`)
+    return { rows }
+  } catch (err) {
+    console.log("Get todos " + err)
+    return { err }
+  }
 }
 
 async function newTodo({ title }) {
-	try {
-		const con = await getConnection()
-		const [result] = await con.query(`INSERT INTO todos (title) VALUES (?)`, [
-			title,
-		])
+  try {
+    const con = await getConnection()
+    const [result] = await con.query(`INSERT INTO todos (title) VALUES (?)`, [
+      title,
+    ])
 
-		const newId = result.insertId
-		const { row } = await getTodoWithId(newId)
-		return { row }
-	} catch (err) {
-		console.log("Insert todo " + err)
-		return { err }
-	}
+    const newId = result.insertId
+    const { row } = await getTodoWithId(newId)
+    return { row }
+  } catch (err) {
+    console.log("Insert todo " + err)
+    return { err }
+  }
 }
 
 async function getTodoWithId(id) {
-	try {
-		const con = await getConnection()
-		const [row] = await con.query(`SELECT * FROM todos WHERE id=?`, [id])
-		return { row }
-	} catch (err) {
-		console.log("Get Todo with id: " + err)
-		return { err }
-	}
+  try {
+    const con = await getConnection()
+    const [row] = await con.query(`SELECT * FROM todos WHERE id=?`, [id])
+    return { row }
+  } catch (err) {
+    console.log("Get Todo with id: " + err)
+    return { err }
+  }
 }
 
-export { getAllTodos, newTodo }
+async function deleteTodoWithId({ id }) {
+  try {
+    const con = await getConnection()
+    const data = await con.query(`DELETE FROM todos WHERE id LIKE ?`, [id])
+    return { data }
+  } catch (err) {
+    console.log("Delete Todo with id: " + err)
+    return { err }
+  }
+}
+
+export { getAllTodos, newTodo, deleteTodoWithId }
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,47 +1,59 @@
 import { Router } from "express"
-import { getAllTodos, newTodo } from "../controllers/controllers.js"
+import {
+  deleteTodoWithId,
+  getAllTodos,
+  newTodo,
+} from "../controllers/controllers.js"
 
 const router = Router()
 
 router.get("/", (req, res) => {
-	const routes = [
-		{
-			method: "GET",
-			route: "/todos",
-			description: "Get all the TODOS",
-		},
-		{
-			method: "POST",
-			route: "/todos",
-			description: "Create a new TODO",
-		},
-	]
+  const routes = [
+    {
+      method: "GET",
+      route: "/todos",
+      description: "Get all the TODOS",
+    },
+    {
+      method: "POST",
+      route: "/todos",
+      description: "Create a new TODO",
+    },
+  ]
 
-	return res.json(routes)
+  return res.json(routes)
 })
 
 router.get("/todos", async (req, res) => {
-	const { rows, err } = await getAllTodos()
+  const { rows, err } = await getAllTodos()
 
-	if (err) {
-		return res.status(404).json({
-			Error: "Error with the SELECT query of the todos table",
-		})
-	}
-	return res.json(rows)
+  if (err) {
+    return res.status(404).json({
+      Error: "Error with the SELECT query of the todos table",
+    })
+  }
+  return res.status(200).json(rows)
 })
 
 router.post("/todos", async (req, res) => {
-	const { title } = req.body
-	const { row, err } = await newTodo({ title })
+  const { title } = req.body
+  const { row, err } = await newTodo({ title })
 
-	if (err) {
-		return res.status(404).json({
-			Error: "Error with the INSERT query of the todos table",
-		})
-	}
+  if (err) {
+    return res.status(404).json({
+      Error: "Error with the INSERT query of the todos table",
+    })
+  }
 
-	return res.json(row)
+  return res.status(200).json(row)
+})
+
+router.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params
+
+  const { data } = await deleteTodoWithId({ id })
+
+  return res.status(200).json({ data })
 })
 
 export { router }

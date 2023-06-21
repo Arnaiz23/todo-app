9d29fbbbbf8ebf31c33d92962ac02742f2eb5db4
Arnaiz23 <aaf110501@gmail.com>
2023-06-21 20:18:33 +0200

    feat: 🎸 Add React and create the login page without functionality

---
+++ frontend/src/components/Count.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const Count = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+    </div>
+  )
+}
+
+export default Count
---
+++ frontend/src/components/FormLogin.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+const FormLogin = () => {
+  const [data, setData] = useState({ email: "", password: "" })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(data)
+  }
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <form
+      className="flex flex-col justify-center items-center gap-3 w-[90%]"
+      onSubmit={handleSubmit}
+    >
+      <label className="flex flex-col justify-start items-start gap-1 w-[90%]">
+        Email
+        <input
+          type="email"
+          name="email"
+          className="rounded p-1 text-black w-full"
+          onChange={handleChange}
+        />
+      </label>
+      <label className="flex flex-col justify-start items-start gap-1 w-[90%]">
+        Password
+        <input
+          type="password"
+          name="password"
+          className="rounded p-1 text-black w-full"
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit" className="bg-cyan-700 p-2 rounded">
+        Login
+      </button>
+    </form>
+  )
+}
+
+export default FormLogin

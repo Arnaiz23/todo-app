8dfeeba7cb5064c4de1fa3e3cf3d29e4ff764278
Arnaiz23 <aaf110501@gmail.com>
2023-06-24 12:53:13 +0200

    feat: 🎸 Delete the unnecessary catch in controllers

--- backend/src/services/Todos.services.js
+++ backend/src/services/Todos.services.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { getConnection } from "../database.js"
 
 const ERROR_MESSAGES = {
@@ -8,109 +9,84 @@ const ERROR_MESSAGES = {
 }
 
 async function getAllTodos({ id }) {
-  try {
-    const con = await getConnection()
-    const [rows] = await con.execute(
-      `SELECT * FROM todos WHERE user_id LIKE ?`,
-      [id]
-    )
-    return rows
-  } catch (err) {
-    throw err
-  }
+  const con = await getConnection()
+  const [rows] = await con.execute(`SELECT * FROM todos WHERE user_id LIKE ?`, [
+    id,
+  ])
+  return rows
 }
 
 async function newTodo({ title, user }) {
-  try {
-    const con = await getConnection()
+  const con = await getConnection()
 
-    const [result] = await con.execute(
-      `INSERT INTO todos (title, user_id) VALUES (?, ?)`,
-      [title, user.id]
-    )
+  const [result] = await con.execute(
+    `INSERT INTO todos (title, user_id) VALUES (?, ?)`,
+    [title, user.id]
+  )
 
-    const newId = result.insertId
-    const row = await getTodoWithId({ id: newId, user })
+  const newId = result.insertId
+  const row = await getTodoWithId({ id: newId, user })
 
-    return row
-  } catch (err) {
-    throw err
-  }
+  return row
 }
 
 async function getTodoWithId({ id, user }) {
-  try {
-    const con = await getConnection()
-    const [row] = await con.execute(
-      `SELECT * FROM todos WHERE id LIKE ? AND user_id LIKE ?`,
-      [id, user.id]
-    )
+  const con = await getConnection()
+  const [row] = await con.execute(
+    `SELECT * FROM todos WHERE id LIKE ? AND user_id LIKE ?`,
+    [id, user.id]
+  )
 
-    if (row.length <= 0) throw new Error(ERROR_MESSAGES.TODO_USER_NOT_EXISTS)
+  if (row.length <= 0) throw new Error(ERROR_MESSAGES.TODO_USER_NOT_EXISTS)
 
-    return row[0]
-  } catch (err) {
-    throw err
-  }
+  return row[0]
 }
 
 async function deleteTodoWithId({ id, user_id }) {
-  try {
-    const con = await getConnection()
-    const [result] = await con.execute(
-      `DELETE FROM todos WHERE id LIKE ? AND user_id LIKE ?`,
-      [id, user_id]
-    )
+  const con = await getConnection()
+  const [result] = await con.execute(
+    `DELETE FROM todos WHERE id LIKE ? AND user_id LIKE ?`,
+    [id, user_id]
+  )
 
-    if (result.affectedRows === 0) {
-      throw new Error(ERROR_MESSAGES.TODO_ID_NOT_EXISTS)
-    }
-
-    return id
-  } catch (err) {
-    throw err
+  if (result.affectedRows === 0) {
+    throw new Error(ERROR_MESSAGES.TODO_ID_NOT_EXISTS)
   }
+
+  return id
 }
 
 async function updateTitleTodo({ id, title, user }) {
-  try {
-    const con = await getConnection()
-    const [data] = await con.execute(
-      `UPDATE todos SET title = ? WHERE id LIKE ? AND user_id LIKE ?`,
-      [title, id, user.id]
-    )
+  const con = await getConnection()
+  const [data] = await con.execute(
+    `UPDATE todos SET title = ? WHERE id LIKE ? AND user_id LIKE ?`,
+    [title, id, user.id]
+  )
 
-    if (data.affectedRows == 0) {
-      throw new Error(ERROR_MESSAGES.SAVE_PROCESS)
-    }
-
-    const row = await getTodoWithId({ id, user })
-
-    return row
-  } catch (err) {
-    throw err
+  if (data.affectedRows === 0) {
+    throw new Error(ERROR_MESSAGES.SAVE_PROCESS)
   }
+
+  const row = await getTodoWithId({ id, user })
+
+  return row
 }
 
 async function toggleCompletedTodos({ id, completed, user }) {
-  try {
-    const isCompleted = completed ? 1 : 0
-    const con = await getConnection()
-    const [outputs] = await con.execute(
-      `UPDATE todos SET completed = ? WHERE id LIKE ? AND user_id LIKE ?`,
-      [isCompleted, id, user.id]
-    )
+  const isCompleted = completed ? 1 : 0
+  const con = await getConnection()
+  const [outputs] = await con.execute(
+    `UPDATE todos SET completed = ? WHERE id LIKE ? AND user_id LIKE ?`,
+    [isCompleted, id, user.id]
+  )
 
-    if (outputs.changedRows === 0) {
-      throw new Error(ERROR_MESSAGES.COMPLETED_SAME)
-    }
-
-    const row = await getTodoWithId({ id, user })
-
-    return row
-  } catch (err) {
-    throw err
+  if (outputs.changedRows === 0) {
+    throw new Error(ERROR_MESSAGES.COMPLETED_SAME)
   }
+
+  const row = await getTodoWithId({ id, user })
+
+  return row
 }
 
 export {

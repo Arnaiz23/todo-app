2ca163486acd4249e4e16ccffda50d0417bf2e5d
Arnaiz23 <aaf110501@gmail.com>
2023-07-02 15:10:50 +0200

    feat: 🎸 Initialize the frontend project and in process with the header component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className="bg-zinc-800 text-white h-screen">
+      <Header />
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="p-4 border-b border-b-white w-full flex justify-between items-center">
+      <h3>Icon</h3>
+      <button className="py-2 px-4 bg-cyan-700 rounded font-bold">Login</button>
+    </header>
+  )
+}
+
+export default Header

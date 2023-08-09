5f2073e43bbd3dc40e4aabae6bdb1e1d2f6912d4
Arnaiz23 <aaf110501@gmail.com>
2023-08-09 18:26:24 +0200

    feat: 🔊 Add the API documentation url when the server start

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,6 @@ import { PORT } from "./src/globalVariables.js"
 import { app } from "./src/app.js"
 
 app.listen(PORT, () => {
-  console.log(`The server is listening in the port: ${PORT}`)
+  console.log(`The server is listening in the port: http://localhost:${PORT}`)
+  console.log(`API Documentation: http://localhost:${PORT}/docs`)
 })
